7563d16a05847fb3aefbd32974e2d1cf6b4beda9
OroszPeter <144907855+OroszPeter@users.noreply.github.com>
2024-12-11 13:30:41 +0100

    For deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/VueFrontend_1/',
   plugins: [vue()],
   define: {
     'process.env.BASE_URL': '"./"'
